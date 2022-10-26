2798ff53b652eb4af5acc53700f7562ffb3a4998
AlbertoMG <172amg@gmail.com>
2022-10-26 20:00:11 +0100

    fix updatowndate

--- api/controllers/date.controller.js
+++ api/controllers/date.controller.js
@@ -77,7 +77,8 @@ async function updateOwnDate (req, res) {
             id: req.params.id
             }        
         })
-        return !date ? res.status(404).send('No date found') : res.status(200).json({message: 'date updated', date})
+        const data = date[0].dataValues
+        return !date ? res.status(404).send('No date found') : res.status(200).json({message: 'date updated', date: data.dataValues})
     } catch (error) {
         return res.status(500).send(error.message)
     }
